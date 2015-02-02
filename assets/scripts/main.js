937cfcde6cb04ae06366a5c3807a7a88c6ef045b
Qianglong Mo <sammok2003@hotmail.com>
2015-02-02 22:22:56 +0800

    Filled index page UI.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,9 +13,56 @@ var index = {
                         .eq(0).addClass('active');
                 });
         }
+    },
+
+    tab: {
+        index: 0,
+        core: function (options){
+            var wrap = $(options.wrap),
+                items = wrap.find(options.items),
+                prev = wrap.find(options.prev),
+                next = wrap.find(options.next);
+
+            // init tab
+            items.eq(0).addClass('active');
+            btnEnable();
+
+            prev.click(function (){
+                if (index.tab.index > 0){
+                    items.hide()
+                        .eq(--index.tab.index).show();
+                    btnEnable();
+                }
+            });
+
+            next.click(function (){
+                if (index.tab.index < items.length-1){
+                    items.hide()
+                        .eq(++index.tab.index).show();
+                    btnEnable();
+                }
+            });
+
+            function btnEnable(){
+                if (index.tab.index == 0 ) {
+                    prev.addClass('enable');
+                } else if (index.tab.index == items.length-1) {
+                    next.addClass('enable');
+                } else {
+                    prev.removeClass('enable');
+                    next.removeClass('enable');
+                }
+            }
+        }
+    },
+
+    indexKv: function (){
+        jQuery(".kv").slide({titCell: '.kv-head ul', mainCell:".kv-body ul", effect:"left", autoPlay:true, autoPage: '<li></li>'});
     }
 };
 
 $(function (){
     index.activity.core();
+    index.tab.core({wrap: '.recommend-components-bd', items: '.itemWrap li', prev: '.prev', next: '.next'});
+    index.indexKv();
 });
