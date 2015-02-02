8c4ed04bad0840f41f15c3a3a50c21e79e81ccdc
Qianglong Mo <sammok2003@hotmail.com>
2015-02-02 21:25:01 +0800

    Updata index chuangke-activity ui.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,21 +2,16 @@
 
 var index = {
     activity: {
-        timer: null,
         core: function (){
-            //$('.chuangke-activity .item').mouseover(function (){
-            //    var that = $(this);
-            //    $(this).addClass('active')
-            //        .siblings('.item').removeClass('active');
-            //    $('.chuangke-activity .item .txt').hide();
-            //
-            //    clearTimeout(index.activity.timer);
-            //    index.activity.timer = setTimeout(function (){
-            //        if (!that.find('.txt').is(':visible')) {
-            //            that.find('.txt').fadeIn();
-            //        }
-            //    });
-            //});
+            $('.chuangke-activity .item').mouseover(function (){
+                var that = $(this);
+                $(this).addClass('active')
+                    .siblings('.item').removeClass('active');
+            })
+                .mouseout(function (){
+                    $('.chuangke-activity .item').removeClass('active')
+                        .eq(0).addClass('active');
+                });
         }
     }
 };
