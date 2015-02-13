c3d2ed141c0617ff77d4163886059405144b8980
Qianglong Mo <sammok2003@hotmail.com>
2015-02-13 22:00:29 +0800

    Add unit-edit-status for project-unit page.

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -6,8 +6,35 @@ var project = {
             $('.redactor-main').redactor();
         }
     },
+    component: {
+        editUnit: function (){
+            // operate text
+            var textArr = ['编辑', '取消'];
+
+            // toggle edit status
+            var unitModule = $('.project-unit');
+            unitModule.find('.text').click(function (){
+                $('.project-unit').toggleClass('status-edit');
+
+                // change operate text
+                var textElem = $(this);
+                if (textElem.text() == textArr[0]){
+                    textElem.text(textArr[1]);
+                } else {
+                    textElem.text(textArr[0]);
+                    unitModule.find('.select-icon').removeClass('active');
+                }
+            });
+
+            // select unit item
+            unitModule.find('.select-icon').click(function (){
+                $(this).toggleClass('active');
+            });
+        }
+    },
     initUi: function (){
         project.comment();
+        project.component.editUnit();
     }
 };
 
