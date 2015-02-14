4f40a93801bb4db9ff4d21a4b935d71eae0e0dc4
Qianglong Mo <sammok2003@hotmail.com>
2015-02-14 20:35:36 +0800

    Filled basic activity-list page.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,22 @@ var app = {
     components: {
         kv: function(){
             jQuery(".m-kv").slide({titCell: '.m-kv-head ul', mainCell:".m-kv-body ul", delayTime: 450, autoPlay:true, autoPage: '<li></li>'});
+        },
+        waterfall: {
+            data:  [
+                {
+                    'title': '凹凸曼刀削面',
+                    'url': 'http://www.kuaibo.com',
+                    'desc': '凹凸曼刀削面，好看，更好吃！',
+                    'imgSrc': ''
+                },
+                {
+                    'title': '凹凸曼刀削面',
+                    'url': 'http://www.kuaibo.com',
+                    'desc': '凹凸曼刀削面，好看，更好吃！',
+                    'imgSrc': ''
+                }
+            ]
         }
     },
     initApp: function (){
