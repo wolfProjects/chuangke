2f38b45f297c03b94deb14a6e7a4eb0c95eb8808
Qianglong Mo <sammok2003@hotmail.com>
2015-02-20 18:39:24 +0800

    Add unit detail page.

--- assets/scripts/unit.js
+++ assets/scripts/unit.js
@@ -2,10 +2,29 @@
 
 var unit = {
     component: {
+        thumbList: function (){
+            $('.summary .thumbList-hd img').eq(0).fadeIn();
+            $('.summary .thumbList-bd li').eq(0).addClass('active');
+            var old = 0;
+
+            $('.summary .thumbList-bd li').click(function (){
+                var currentIndex = $(this).index();
+                if (currentIndex != old && currentIndex >=0) {
+                    $('.summary .thumbList-hd img').hide().removeClass('active');
+                    $('.summary .thumbList-hd img').eq(currentIndex).fadeIn();
+                    $(this).addClass('active')
+                        .siblings('li').removeClass('active');
+                    old = currentIndex;
+                } else {
+                }
+            });
+        }
     },
     initUi: function (){
+        unit.component.thumbList();
     }
 };
 
 $(function (){
+    unit.initUi();
 });
