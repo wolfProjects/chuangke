33a77214c599ac1d4508ffb4298c3001cd744aed
Qianglong Mo <sammok2003@hotmail.com>
2015-02-27 12:05:46 +0800

    Write project-unit content for project-admin page.

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -40,12 +40,56 @@ var project = {
                 $('.logic-bd .item').eq($(this).parents('li').index()).fadeIn()
                     .siblings('.item').hide();
             });
+        },
+        editLogicUnit: function (){
+            // operate text
+            var textArr = ['更改项目套件', '完成编辑'];
+            var isEditing = false;
+
+            // switch editing status
+            $('.changeUnit').click(function (){
+                $('.logic-item-unit').addClass('editing');
+
+                var textElem = $(this);
+                // toggle edit status
+                if (textElem.text() == textArr[0]){
+                    // editing....
+                    textElem.text(textArr[1]);
+                    isEditing = true;
+                    $('.unitSelect').show();
+                } else {
+                    // editing complete..
+                    textElem.text(textArr[0]);
+                    isEditing = false;
+                    $('.unitSelect').hide();
+                    resetSelect();
+                }
+            });
+
+            // bind select
+            $('.project-unit-list .thumb').click(function (){
+                // check isEditing status
+                if (isEditing) {
+                    if ($(this).parent('.item').hasClass('selected')) {
+                        $(this).parent('.item').removeClass('selected');
+                    } else {
+                        $(this).parent('.item').addClass('selected');
+                    }
+                }
+            });
+
+            // reset select
+            function resetSelect (){
+                $('.project-unit-list .item').removeClass('selected');
+            }
         }
     },
     initUi: function (){
         project.comment();
         project.component.editUnit();
         project.component.logicTab();
+        project.component.logicTab();
+        project.component.editLogicUnit();
     }
 };
 
