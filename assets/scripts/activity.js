c0e51b719832a26c569e4cac9b91bba301b5c1b9
Qianglong Mo <sammok2003@hotmail.com>
2015-03-27 11:26:04 +0800

    Add pubuliu plugin for activity list page.

--- assets/scripts/activity.js
+++ assets/scripts/activity.js
@@ -2,10 +2,38 @@
 
 var activity = {
     component: {
+        pubuliu: function (){
+            //blocksit define
+            $('.m-project-list-waterfall').BlocksIt({
+                numOfCol: 3,
+                offsetX: 18,
+                offsetY: 18
+            });
+
+            //window resize
+            var currentWidth = 1215;
+            $(window).resize(function() {
+                var winWidth = $(window).width(),
+                    conWidth,
+                    col = 3;
+
+                if(conWidth != currentWidth) {
+                    currentWidth = conWidth;
+                    $('.m-project-list-waterfall').width(conWidth);
+                    $('.m-project-list-waterfall').BlocksIt({
+                        numOfCol: col,
+                        offsetX: 8,
+                        offsetY: 8
+                    });
+                }
+            });
+        }
     },
     initUi: function (){
+        activity.component.pubuliu();
     }
 };
 
 $(function (){
+    activity.initUi();
 });
